096914df021aa02d0fa83e9644996369e794a8a0
Borgeah Matongo <b2jtheone@gmail.com>
2023-03-23 07:59:04 +0100

    Finalisation du projet sur les bases du c# et création du projet sur la POO

---
+++ ConsoleApp1/ConsoleApp2_OOP/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp2_OOP;
+
+Student student = new Student();
+
+Student student1 = new Student("Borgeah", "Atwood");
+Console.WriteLine(student1.GetFullName());
---
+++ ConsoleApp1/ConsoleApp2_OOP/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_OOP
+{
+    public class Student
+    {
+        private string _name;
+        private string _lastName;
+
+        public string Name  // public string Name { get; set }
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string LastName // public string LastName { get; set }
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public Student(string name, string lastName)
+        {
+            _name = name;
+            _lastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            return _name + " " + _lastName;
+        }
+    }
+
+    /*partial class Student
+    {
+        private string _name;
+        private string _lastName;
+
+        public Student()
+        {
+            _name = string.Empty;
+            _lastName = string.Empty;
+        }
+    }
+
+    partial class Student
+    {
+        public Student(string name, string lastName)
+        {
+            _name = name;
+            _lastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            return _name + " " + _lastName;
+        }
+    }*/
+}
